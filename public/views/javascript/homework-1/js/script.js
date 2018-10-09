ff387be681772a0e39c7fcbbf7aa46064df3bc43
DDavshan <ddavshan@live.ru>
2018-10-09 11:19:23 +0300

    js hw1/2

---
+++ public/views/javascript/homework-1/js/script.js
@@ -0,0 +1,19 @@
+'use strict';
+//Task 1
+var admin;
+var name;
+
+name = 'Василий';
+admin = name;
+
+console.log(admin);
+
+//Task 2
+var namePlanet = 'Земля';
+var nameVisitor = 'Петя';
+
+//Task 3
+var framesPerSecond;
+var wordsConSign;
+var documentView;
+var lastPageNavRequire;
